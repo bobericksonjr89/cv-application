9f6bc283d6dc61c183c9297eb61b735a1f48df4f
Robert Erickson <bobericksonjr@gmail.com>
2021-09-21 15:55:06 -0400

    changes experience section from class to functional components

--- src/components/Experience-Job.js
+++ src/components/Experience-Job.js
@@ -1,19 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Job extends React.Component {
-  constructor(props) {
-    super(props);
+const Job = (props) => {
+  const [company, setCompany] = useState(props.job.company);
+  const [time, setTime] = useState(props.job.time);
+  const [title, setTitle] = useState(props.job.title);
+  const [description, setDescription] = useState(props.job.description);
+  const [isEditing, setIsEditing] = useState(false);
 
-    this.state = {
-      company: this.props.job.company,
-      time: this.props.job.time,
-      title: this.props.job.title,
-      description: this.props.job.description,
-      editing: false,
-    };
-  }
-
-  handleClick = (e) => {
+  /*   const handleClick = (e) => {
     const category = e.target.dataset.category;
     if (category === "save") {
       this.setState({
@@ -30,114 +24,137 @@ class Job extends React.Component {
     if (category === "delete") {
       this.props.deleteJob(e.target.dataset.key);
     }
-  };
+  }; */
 
-  handleFocus = () => {
-    this.setState({
-      editing: true,
+  const saveJob = () => {
+    setIsEditing(false);
+    props.saveJob({
+      company: company,
+      time: time,
+      title: title,
+      description: description,
+      key: props.job.key,
     });
   };
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.dataset.category]: e.target.value,
-    });
+  const passToDeleteJob = (e) => {
+    props.deleteJob(e.target.dataset.key);
   };
 
-  handleAreaSize = (e) => {
+  const handleFocus = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleChange = (e) => {
+    const category = e.target.dataset.category;
+    const value = e.target.value;
+
+    if (category === "company") {
+      setCompany(value);
+    }
+
+    if (category === "time") {
+      setTime(value);
+    }
+
+    if (category === "title") {
+      setTitle(value);
+    }
+
+    if (category === "description") {
+      setDescription(value);
+    }
+  };
+
+  const handleAreaSize = (e) => {
     e.target.parentNode.dataset.replicatedValue = e.target.value;
   };
 
-  render() {
-    const editing = this.state.editing;
-    let job;
-    if (editing) {
-      job = (
-        <form className="experience__form">
-          <input
-            className="experience__company--input"
-            onChange={this.handleChange}
-            data-category="company"
-            value={this.state.company}
-            autoFocus
+  if (isEditing) {
+    return (
+      <form className="experience__form">
+        <input
+          className="experience__company--input"
+          onChange={handleChange}
+          data-category="company"
+          value={company}
+          autoFocus
+          onFocus={(e) => e.currentTarget.select()}
+        />
+        <input
+          className="experience__time--input"
+          onChange={handleChange}
+          data-category="time"
+          value={time}
+          onFocus={(e) => e.currentTarget.select()}
+        />
+        <input
+          className="experience__title--input"
+          onChange={handleChange}
+          data-category="title"
+          value={title}
+          onFocus={(e) => e.currentTarget.select()}
+        />
+        <div className="experience__wrap">
+          <textarea
+            className="experience__description--input"
+            onChange={handleChange}
+            data-category="description"
+            value={description}
             onFocus={(e) => e.currentTarget.select()}
-          />
-          <input
-            className="experience__time--input"
-            onChange={this.handleChange}
-            data-category="time"
-            value={this.state.time}
-            onFocus={(e) => e.currentTarget.select()}
-          />
-          <input
-            className="experience__title--input"
-            onChange={this.handleChange}
-            data-category="title"
-            value={this.state.title}
-            onFocus={(e) => e.currentTarget.select()}
-          />
-          <div className="experience__wrap">
-            <textarea
-              className="experience__description--input"
-              onChange={this.handleChange}
-              data-category="description"
-              value={this.state.description}
-              onFocus={(e) => e.currentTarget.select()}
-              onInput={this.handleAreaSize}
-            ></textarea>
-          </div>
-          <div className="experience__icons">
-            <i
-              data-key={this.props.job.key}
-              data-category="save"
-              className="fas fa-check experience__save"
-              onClick={this.handleClick}
-            ></i>
-            <i
-              data-key={this.props.job.key}
-              data-category="delete"
-              className="fas fa-trash experience__delete"
-              onClick={this.handleClick}
-            ></i>
-          </div>
-        </form>
-      );
-    } else {
-      job = (
-        <div tabIndex="0" className="experience__info">
-          <h4
-            className="experience__company"
-            onFocus={this.handleFocus}
-            onClick={this.handleFocus}
-          >
-            {this.state.company}
-          </h4>
-          <p
-            className="experience__time"
-            onFocus={this.handleFocus}
-            onClick={this.handleFocus}
-          >
-            {this.state.time}
-          </p>
-          <p
-            className="experience__title"
-            onFocus={this.handleFocus}
-            onClick={this.handleFocus}
-          >
-            {this.state.title}
-          </p>
-          <p
-            className="experience__description"
-            onFocus={this.handleFocus}
-            onClick={this.handleFocus}
-          >
-            {this.state.description}
-          </p>
+            onInput={handleAreaSize}
+          ></textarea>
         </div>
-      );
-    }
-    return job;
+        <div className="experience__icons">
+          <i
+            data-key={props.job.key}
+            data-category="save"
+            className="fas fa-check experience__save"
+            onClick={saveJob}
+          ></i>
+          <i
+            data-key={props.job.key}
+            data-category="delete"
+            className="fas fa-trash experience__delete"
+            onClick={passToDeleteJob}
+          ></i>
+        </div>
+      </form>
+    );
+  } else {
+    return (
+      <div tabIndex="0" className="experience__info">
+        <h4
+          className="experience__company"
+          onFocus={handleFocus}
+          onClick={handleFocus}
+        >
+          {company}
+        </h4>
+        <p
+          className="experience__time"
+          onFocus={handleFocus}
+          onClick={handleFocus}
+        >
+          {time}
+        </p>
+        <p
+          className="experience__title"
+          onFocus={handleFocus}
+          onClick={handleFocus}
+        >
+          {title}
+        </p>
+        <p
+          className="experience__description"
+          onFocus={handleFocus}
+          onClick={handleFocus}
+        >
+          {description}
+        </p>
+      </div>
+    );
   }
-}
+};
 
 export default Job;
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,76 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import uniqid from "uniqid";
 import Job from "./Experience-Job";
 import "../styles/Experience.css";
 
-class Experience extends React.Component {
-  constructor() {
-    super();
+const Experience = () => {
+  const [jobs, setJobs] = useState([
+    {
+      company: "Company",
+      time: "Time",
+      title: "Title",
+      description: "Description",
+      key: uniqid(),
+    },
+  ]);
 
-    this.state = {
-      jobs: [
-        {
-          company: "Company",
-          time: "Time",
-          title: "Title",
-          description: "Description",
-          key: uniqid(),
-        },
-      ],
-    };
-  }
-
-  handleClick = () => {
-    this.setState({
-      jobs: this.state.jobs.concat([
-        {
-          company: "Company",
-          time: "Time",
-          title: "Title",
-          description: "Description",
-          key: uniqid(),
-        },
-      ]),
-    });
+  const handleClick = () => {
+    setJobs([
+      ...jobs,
+      {
+        company: "Company",
+        time: "Time",
+        title: "Title",
+        description: "Description",
+        key: uniqid(),
+      },
+    ]);
   };
 
-  saveJob = (job) => {
-    const updatedJobs = this.state.jobs.map((oldJob) => {
+  const saveJob = (job) => {
+    const updatedJobs = jobs.map((oldJob) => {
       if (oldJob.key === job.key) {
         oldJob = job;
       }
       return oldJob;
     });
-    this.setState({
-      jobs: updatedJobs,
-    });
+    setJobs(updatedJobs);
   };
 
-  deleteJob = (jobKey) => {
-    const updatedJobs = this.state.jobs.filter((job) => job.key !== jobKey);
-    this.setState({
-      jobs: updatedJobs,
-    });
+  const deleteJob = (jobKey) => {
+    const updatedJobs = jobs.filter((job) => job.key !== jobKey);
+    setJobs(updatedJobs);
   };
 
-  render() {
-    const jobs = this.state.jobs.map((job) => (
-      <Job
-        key={job.key}
-        job={job}
-        deleteJob={this.deleteJob}
-        saveJob={this.saveJob}
-      />
-    ));
-    return (
-      <div>
-        <h3 className="experience__header" onClick={this.handleClick}>
-          Experience
-        </h3>
-        {jobs}
-      </div>
-    );
-  }
-}
+  const jobComponents = jobs.map((job) => (
+    <Job key={job.key} job={job} deleteJob={deleteJob} saveJob={saveJob} />
+  ));
+
+  return (
+    <div>
+      <h3 className="experience__header" onClick={handleClick}>
+        Experience
+      </h3>
+      {jobComponents}
+    </div>
+  );
+};
 
 export default Experience;
